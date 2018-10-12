865fde3e19db5b2aaf3a2a0d668e192f99c6336c
mirandadam <mirandadam@users.noreply.github.com>
2018-10-12 07:23:55 -0300

    make installation scripts less version dependent

--- 1_download_source.sh
+++ 1_download_source.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-wget -c http://www.w1hkj.com/files/fldigi/fldigi-current.tar.gz
+wget -c http://www.w1hkj.com/files/fldigi/fldigi-4.0.18.tar.gz
 wget -c http://www.w1hkj.com/files/flrig/flrig-1.3.40.tar.gz
 wget -c http://www.w1hkj.com/files/test_suite/linsim-2.0.3.tar.gz
-tar --one-top-level -xzvf fldigi-current.tar.gz
-tar --one-top-level -xzvf flrig-1.3.40.tar.gz
-tar -xzvf linsim-2.0.3.tar.gz
+tar -xzvf fldigi-*.tar.gz
+tar -xzvf flrig-*.tar.gz
+tar -xzvf linsim-*.tar.gz
--- 2_compile.sh
+++ 2_compile.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-cd fldigi-current/*
+cd fldigi-*/
 autoreconf
 automake --add-missing
 autoreconf
@@ -14,14 +14,14 @@ autoreconf
 #cannot find -lXinerama
 make -j $(nproc --all)
 
-cd ../../flrig-1.3.40/
+cd ../flrig-*
 autoreconf
 automake --add-missing
 autoreconf
 ./configure --enable-optimizations=native --with-flxmlrpc --with-pulseaudio
 make -j $(nproc --all)
 
-cd ../linsim-2.0.3
+cd ../linsim-*
 autoreconf
 automake --add-missing
 autoreconf
